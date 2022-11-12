9a56a6281dc1b7dcb944000dcb33cabb8ddaeba0
Keshav1411 <dhadhwalkeshav@gmail.com>
2022-11-12 16:45:42 +0530

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,22 @@
+/*  First Page 
+const amount =12;
+
+if(amount<10){
+    console.log('small number');
+}
+else{
+    console.log('large number');
+}
+
+console.log(`hey it my first node app!!!!`);
+*/
+
+
+//Second Page
+/*
+console.log(__dirname);
+setInterval(()=>{
+    console.log('Hello Keshav');
+},1000)
+*/
+
---
+++ 2-module.js
@@ -0,0 +1,10 @@
+// Modules
+// 4-NAME.JS CONNECTED
+// 5-UTILS.JS CONNECTED
+
+const names = require('./4-name');
+const sayHi = require('./5-utils');
+ 
+sayHi('susan');
+sayHi(names.john);
+sayHi(names.peter);
